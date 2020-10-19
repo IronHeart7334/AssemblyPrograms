a9744f101e7fa5138aac39959842cf2d8f69262f
IronHeart7334 <greengrappler12@gmail.com>
2020-10-19 11:18:50 -0700

    yeah, I don't know how to do exactly what I want with the C program, so I may have to do it differenly

--- asm/pasteAllAsmTogether.c
+++ asm/pasteAllAsmTogether.c
@@ -2,6 +2,8 @@
 
 int main();
 void forEachLineIn(char* fName, void consumer(char*));
+void concatFiles(char** fileNames, int numFiles);
+(*void)(char* newLine) createBasicAppender(FILE* file);
 void printLn(char* ln);
 
 
@@ -21,6 +23,19 @@ void forEachLineIn(char* fName, void consumer(char*)){
         consumer(currLine);
     }
     fclose(fileToRead);
+}
+void concatFiles(char** fileNames, int numFiles, (*void)(char* newLine) appender(FILE* appendToMe)){
+    FILE* resultFile = fopen("result.txt", "w");
+    int currFileIdx = 0;
+    while(currFileIdx < numFiles){
+        forEachLineIn(fileNames[currFileIdx], appender(resultFile));
+        currFileIdx++;
+    }
+    fclose(resultFile);
+}
+(*void)(char* newLine) createBasicAppender(FILE* file){
+    // wait... I don't know how to do this
+    return 0;
 }
 void printLn(char* ln){
     printf("%s\n", ln);
