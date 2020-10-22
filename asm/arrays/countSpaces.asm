ff95c16b9855f24f1ccbc759a38a1da007c12824
IronHeart7334 <greengrappler12@gmail.com>
2020-10-22 16:10:51 -0700

    I'll have to test the string one, I'm not sure it works

---
+++ asm/arrays/countSpaces.asm
@@ -0,0 +1,55 @@
+; general comments
+;   Create a program that creates and initializes a C-string-style ASCII array.
+;   Determine the length of the string and store the answer in DH.
+;   Determine the number of spaces in the string and store the answer in DL.
+;   Verify that your program works for the empty string,
+;   one that has space(s) at the beginning and end,
+;   and one that has many spaces spread throughout.
+; preprocessor directives
+.586
+.MODEL FLAT
+
+; external files to link with
+
+; stack configuration
+.STACK 4096
+
+; named memory allocation and initialization
+.DATA
+cString BYTE "Hello world!", 0
+
+; names of procedures defined in other *.asm files in the project
+
+; procedure code
+.CODE
+main	PROC
+    mov DL, 0d ; number of spaces
+    lea EBX, cString
+    mov CL, 0d
+
+    checkForNullTerminator:
+        mov AL, BYTE PTR [EBX + 1*CL]
+        cmp AL, 0d
+        je itsNullTerminator
+        jmp nextChar
+    nextChar:
+        mov AL, BYTE PTR [EBX + 1*CL]
+        cmp AL, ' ' ; is this how I check for spaces?
+        je itsASpace
+        jmp notASpace
+        itsASpace:
+            inc DL
+            jmp nextPartOfLoop
+        notASpace:
+            jmp nextPartOfLoop
+        nextPartOfLoop:
+            inc CL
+            jmp checkForNullTerminator
+    itsNullTerminator:
+        mov DH, CL ; length of the string is the index of the null terminator
+
+	mov EAX, 0
+	ret
+main	ENDP
+
+END
