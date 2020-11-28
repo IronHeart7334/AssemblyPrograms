6b618bdfca87e1eb069bceb09056ce97d58fff4b
IronHeart7334 <greengrappler12@gmail.com>
2020-11-28 12:48:50 -0800

    assembly is so much fun!!!

---
+++ asm/bitwise/bitwiseCopyPaste.asm
@@ -0,0 +1,48 @@
+; general comments
+;	An example of how to copy and paste bits from one integer to another
+
+; preprocessor directives
+.586
+.MODEL FLAT
+
+; external files to link with
+
+; stack configuration
+.STACK 4096
+
+; named memory allocation and initialization
+.DATA
+copyHere            BYTE 1011 0111 b ; can use larger size if you want
+pasteHere           BYTE 1001 1101 b
+maskIsolateBits2To5 BYTE 0011 1100 b ; use this to copy
+maskClearBits0To3   BYTE 1111 0000 b ; use this before pasting
+result              BYTE 0000 0000 b
+
+; names of procedures defined in other *.asm files in the project
+
+; procedure code
+.CODE
+main	PROC
+	; Instruction               | AL        | AH
+	mov AL, copyHere            ; 1011 0111   ~~~~ ~~~~
+    mov AH, pasteHere           ; 1011 0111   1001 1101
+	;     1011 0111
+	; and 0011 1100
+	; is  0011 0100
+	and AL, maskIsolateBits2To5 ; 0011 0100   1001 1101 Copy...
+	;     1001 1101
+	; and 1111 0000
+	; is  1001 0000
+	and AH, maskClearBits0To3   ; 0011 0100   1001 0000 Clear...
+	shr AL, 2d                  ; 0000 1101   1001 0000 Shift the "clipboard" into position...
+	;     1001 0000
+	; or  0000 1101
+	; is  1001 1101
+	or AH, AL                   ; 0000 1101   1001 1101 Paste
+	mov result, AH
+
+	mov EAX, 0
+	ret
+main	ENDP
+
+END
