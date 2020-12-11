2e56d6a57a3250db3c663403de62316ccaf42e68
IronHeart7334 <greengrappler12@gmail.com>
2020-12-11 13:12:19 -0800

    I'm really not feeling up to much

--- asm/forFun/rpg.main.asm
+++ asm/forFun/rpg.main.asm
@@ -26,6 +26,10 @@ person2Hp   BYTE  15d
 person2Type BYTE  "NULL"
 longNumber2 DWORD 99h
 
+; Better struct idea
+person1 DWORD 2 dup(0) ; Allocate 64 bits per person
+person2 DWORD 2 dup(0)
+
 ; these will need to change if the order of properties above changes
 idOffset    DWORD  0d ; ID is 0 bytes away from the start of the struct
 offOffset   DWORD  1d ; ID takes 1 byte, so offense occupies the next one
@@ -86,5 +90,78 @@ main	PROC
 	mov EAX, 0
 	ret
 main	ENDP
+
+; I'm not sure how structs are really represented in memory, but I can guess.
+; 	For this program, I will store each Person "struct" as a quadword like so:
+;	- first 8 bytes are HP
+;	- second 8 bytes are offense
+;	As this gets more complex, I'll be adding more attributes, using the rest of the allocated space
+
+
+
+; void newPerson(Person*, int hp, int off);
+;	Populates a quadword with data
+newPerson PROC
+	; establish stack frame
+	push EBP
+	mov EBP, ESP
+
+	; save registers
+	pushfd
+	push EBX
+
+	; Stack:
+	; [garbage]
+	; [off    ]
+	; [hp     ]
+	; [Person*]
+	; [return ]
+	; [old EBP] <-- EBP
+	; [EFLAGS ]
+	; [EBX    ] <-- ESP
+
+	; Now for the actual procedure
+	mov EBX, DWORD PTR [EBP + 4*2]                  ; EBX will hold "this" pointer
+	mov BYTE PTR [EBX], DWORD PTR [EBP + 4*3]       ; this->hp = hp
+	mov BYTE PTR [EBX + 1*1], DWORD PTR [EBP + 4*4] ; this->off = off
+
+	; restore registers
+	pop EBX
+	popfd
+
+	; remove stack frame
+	pop EBP
+
+	ret
+newPerson ENDP
+
+; int getHp(Person* this)
+getHp PROC
+	; set up stack frame
+	push EBP
+	mov EBP, ESP
+
+	; save registers
+	pushfd
+
+	; Stack:
+	; [garbage]
+	; [this   ]
+	; [old EBP] <-- EBP
+	; [EFLAGS ] <-- ESP
+
+	; actual procedure
+	mov EAX, DWORD PTR [EBP + 4*1] ; EAX now contains this pointer
+	mov EAX, DWORD PTR [EAX]       ; EAX now contains the actual "object"
+	shr EAX, 24d                   ; first 8 bits are HP, so move them to AL, setting others to 0
+
+	; restore registers
+	popfd
+
+	; take down stack frame
+	pop EBP
+
+	ret
+getHp ENDP
 
 END
